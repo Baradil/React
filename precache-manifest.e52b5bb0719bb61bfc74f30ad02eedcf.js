ed183c73cb5b71ff743ad462d4d9656d0c403944
Baradil <54547833+Baradil@users.noreply.github.com>
2020-06-04 14:44:07 +0200

    Updates

---
+++ precache-manifest.e52b5bb0719bb61bfc74f30ad02eedcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1879d95025f2b5bbb0b25ec90a449a14",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "cb3d3aa8a68f53620347",
+    "url": "/React/static/css/main.8fae3c5e.chunk.css"
+  },
+  {
+    "revision": "c22ce226bc9b079ee3d5",
+    "url": "/React/static/js/2.b0ebaed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.b0ebaed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3d3aa8a68f53620347",
+    "url": "/React/static/js/main.a39465f9.chunk.js"
+  },
+  {
+    "revision": "fde1abaeb74777bb60b6",
+    "url": "/React/static/js/runtime-main.89c78b35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
